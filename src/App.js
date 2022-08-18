c4cd99eb8726ae603612c3822e65c78b3b3cc5ad
utkarsh <utkarsh1172@gmail.com>
2022-08-18 08:57:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Hero from './components/Herosection/Hero';
+import Programs from './components/Programs/Programs';
+
+
+
+function App() {
+  return (
+    <>  
+  <div className="App">
+      <Hero/>
+      <Programs/>
+     </div>
+  </>
+
+  );
+}
+
+export default App;
